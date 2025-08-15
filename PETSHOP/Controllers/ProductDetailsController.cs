2464d45c126d6079a2244775baeeeef70cd02cf4
haci1994 <65703054+haci1994@users.noreply.github.com>
2025-08-15 22:23:15 +0400

    Added Single product page and functionality

--- PETSHOP/Controllers/ProductDetailsController.cs
+++ PETSHOP/Controllers/ProductDetailsController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PETSHOP.DataContext;
+using PETSHOP.Models;
 
 namespace PETSHOP.Controllers
 {
     public class ProductDetailsController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _dbContext;
+
+        public ProductDetailsController(AppDbContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var product = _dbContext
+                .Products
+                .Include(z => z.Category)
+                .Include(t=> t.Images)
+                .Include(h => h.ProductTags)
+                .ThenInclude(y => y.Tag)
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            var model = new SingleProductModel
+            {
+                Category = product.Category.Name,
+                CoverImageUrl = product.CoverImageUrl,
+                Name = product.Name,
+                Id = product.Id,
+                Price = product.Price,
+                Images = product.Images,
+                Tags = product.ProductTags
+            };
+
+
+            return View(model);
         }
     }
 }
---
+++ PETSHOP/Models/SingleProductModel.cs
@@ -0,0 +1,15 @@
+﻿using PETSHOP.DataContext.Entities;
+
+namespace PETSHOP.Models
+{
+    public class SingleProductModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string CoverImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+        public List<ProductImage> Images { get; set; } = [];
+        public List<ProductTag> Tags { get; set; } = [];
+    }
+}
